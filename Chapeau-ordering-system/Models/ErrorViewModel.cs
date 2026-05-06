b9f5a9623c8d465bd8beff2567110c72ed18f83c
Rakib730066 <730066@student.inholland.nl>
2026-05-06 23:26:27 +0200

    Add project files.

---
+++ Chapeau-ordering-system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chapeau_ordering_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
