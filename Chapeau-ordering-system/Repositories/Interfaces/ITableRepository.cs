c64a87396eff5c3b22265bc9e2c2369a43e6ec41
Rakib730066 <730066@student.inholland.nl>
2026-05-07 01:51:53 +0200

    n

--- Chapeau-ordering-system/Repositories/Interfaces/ITableRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace Chapeau_ordering_system.Repositories.Interfaces
-{
-    public interface ITableRepository
-    {
-        Task<List<Table>> GetAllTablesAsync();
-        Task<Table> GetTableByIdAsync(int id);
-    }
-}
--- Chapeau-ordering-system/Services/Interfaces/ITableService.cs
+++
@@ -1,8 +0,0 @@
-namespace Chapeau_ordering_system.Services.Interfaces
-{
-    public interface ITableService
-    {
-        Task<List<Table>> GetAllTablesAsync();
-        Task<Table> GetTableByIdAsync(int id);
-    }
-}
