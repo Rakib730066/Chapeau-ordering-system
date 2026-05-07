87cd54e30618d58f8862f84d09d04f6d82abb47a
Rakib730066 <730066@student.inholland.nl>
2026-05-07 04:16:29 +0200

    up

---
+++ Chapeau-ordering-system/Models/Enums/OrderItemStatus.cs
@@ -0,0 +1,11 @@
+namespace Chapeau_ordering_system.Models.Enums
+{
+    public enum OrderItemStatus
+    {
+        Ordered = 1,
+        BeingPrepared = 2,
+        ReadyToBeServed = 3,
+        Served = 4,
+        Cancelled = 5
+    }
+}
