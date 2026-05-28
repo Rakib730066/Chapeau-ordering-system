06c0f1398e52b18df64bcd64b19345ee924d1707
Shiva Lamichhane <721254@student.inholland.nl>
2026-05-29 00:37:27 +0200

    feat: add ErrorMessage property to TakeOrderViewModel

--- Chapeau-ordering-system/ViewModels/TakeOrderViewModel.cs
+++ Chapeau-ordering-system/ViewModels/TakeOrderViewModel.cs
@@ -4,19 +4,15 @@ namespace Chapeau_ordering_system.ViewModels
 {
     public class TakeOrderViewModel
     {
-        // The menu items to choose from (with filters)
         public MenuViewModel Menu { get; set; } = new MenuViewModel();
 
-        // The items currently added to this order (stored in session)
         public List<OrderItem> CurrentItems { get; set; } = new List<OrderItem>();
 
-        // The order id once started
         public int OrderId { get; set; }
 
-        // Confirmation message after saving
-        public string? ConfirmationMessage { get; set; }
-
-        // Total price of current items
         public decimal TotalPrice => CurrentItems.Sum(i => i.MenuItem!.Price * i.Quantity);
+
+        public string? ConfirmationMessage { get; set; }
+        public string? ErrorMessage { get; set; }
     }
 }
