b0c1d9b1e4d5f6594417b2e04469fc2b5500073a
Shiva Lamichhane <721254@student.inholland.nl>
2026-05-28 09:41:36 +0200

    feat: add MenuViewModel and OrderController Menu action

---
+++ Chapeau-ordering-system/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Chapeau_ordering_system.Models.Enums;
+using Chapeau_ordering_system.Services.Interfaces;
+using Chapeau_ordering_system.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapeau_ordering_system.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IMenuItemService _menuItemService;
+
+        public OrderController(IMenuItemService menuItemService)
+        {
+            _menuItemService = menuItemService;
+        }
+
+        // Show the menu for a specific table, with optional filters
+        // tableId: which table the waiter is taking an order for
+        // type: filter by card (Drink=2 / Food=1), null = show all
+        // course: filter by course (Starter, Main, etc.), null = show all
+        [HttpGet]
+        public IActionResult Menu(int tableId, MenuItemType? type, CourseType? course)
+        {
+            MenuViewModel viewModel = new MenuViewModel
+            {
+                MenuItems = _menuItemService.GetFilteredMenuItems(type, course),
+                ActiveType = type,
+                ActiveCourse = course,
+                TableId = tableId
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Chapeau-ordering-system/ViewModels/MenuViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Chapeau_ordering_system.Models;
+using Chapeau_ordering_system.Models.Enums;
+
+namespace Chapeau_ordering_system.ViewModels
+{
+    public class MenuViewModel
+    {
+        // The list of menu items to display (already filtered)
+        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+
+        // The currently active filter values (used to highlight the active button)
+        public MenuItemType? ActiveType { get; set; }
+        public CourseType? ActiveCourse { get; set; }
+
+        // The table this order is for (passed through from restaurant overview)
+        public int TableId { get; set; }
+    }
+}
