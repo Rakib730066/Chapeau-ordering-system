01638aeb5e205d3d97502311434fa8c7fe45c1de
Shiva Lamichhane <721254@student.inholland.nl>
2026-06-01 14:20:05 +0200

    feat: add OrderController with Menu and TakeOrder actions and views

--- Chapeau-ordering-system/ViewModels/MenuViewModel.cs
+++ Chapeau-ordering-system/ViewModels/MenuViewModel.cs
@@ -5,14 +5,11 @@ namespace Chapeau_ordering_system.ViewModels
 {
     public class MenuViewModel
     {
-        // The list of menu items to display (already filtered)
         public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
-
-        // The currently active filter values (used to highlight the active button)
         public MenuItemType? ActiveType { get; set; }
         public CourseType? ActiveCourse { get; set; }
+        public CardType? ActiveCard { get; set; }
 
-        // The table this order is for (passed through from restaurant overview)
         public int TableId { get; set; }
     }
 }
