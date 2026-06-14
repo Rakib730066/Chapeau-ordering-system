a7d009f73c30785fff5d210a3851773d2b88dd80
ghullammuhammad <731326@student.inholland.nl>
2026-06-14 17:06:29 +0200

    tip function added for single and split payment

--- Chapeau-ordering-system/Services/Interfaces/IPaymentService.cs
+++ Chapeau-ordering-system/Services/Interfaces/IPaymentService.cs
@@ -1,4 +1,5 @@
 ﻿using Chapeau_ordering_system.ViewModels;
+
 namespace Chapeau_ordering_system.Services.Interfaces
 {
     public interface IPaymentService
@@ -6,8 +7,8 @@ namespace Chapeau_ordering_system.Services.Interfaces
         OrderPaymentViewModel? GetOrderForPayment(int tableId);
         FinishOrderViewModel? GetFinishOrderViewModel(int tableId);
         void FinishOrder(FinishOrderViewModel input);
-
         SplitPaymentViewModel? GetSplitPaymentViewModel(int tableId);
+        SplitPaymentViewModel RebuildEqualSplit(SplitPaymentViewModel input);
         (bool success, string? errorMessage) FinishSplitOrder(SplitPaymentViewModel input);
     }
 }
